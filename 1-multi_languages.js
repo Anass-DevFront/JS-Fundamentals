b0bfd33bdb3a37e2b3c24270eb5efa0a6a9e3c6a
Anass Elmoussaoui <anasselmoussaoui97@gmail.com>
2025-07-03 14:19:37 +0100

    Script that prints 3 lines

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,17 +1,5 @@
-/* You can do this exercise with different way to print three sentences the console using JavaScript in a two different ways:
- 
- * 1. By creating separate string variables and log each one individually
- * 2. By putting those strings into an array and access them by their index
- */
-
-// First way: separate variables
-
-let line1 = 'C is fun', line2 = 'Python is cool', line3 = 'JavaScript is amazing';
-console.log(line1);
-console.log(line2);
-console.log(line3);
-
-// second way : array with direct access
+// Store the three sentences in an array
+// Use console.log to print all sentences on separate lines by joining them with '\n'
 
 let MyVariables = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 console.log(MyVariables[0] + '\n' + MyVariables[1] + '\n' + MyVariables[2]);
