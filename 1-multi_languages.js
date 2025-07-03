a3116c171bee78c4d72b922360046137162f1aed
Anass Elmoussaoui <anasselmoussaoui97@gmail.com>
2025-07-03 14:08:14 +0100

    Script that prints 3 lines

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,11 +1,7 @@
-/* You can do this exercise with different way to print three sentences the console using JavaScript in a few different ways:
+/* You can do this exercise with different way to print three sentences the console using JavaScript in a two different ways:
  
  * 1. By creating separate string variables and log each one individually
  * 2. By putting those strings into an array and access them by their index
- * 3. By using a for loop to go through the array and print each sentence
- * 4. might try out the forEach() method to display each item.
- 
- *Each approach gives you the same result while highlighting different JavaScript skills like handling arrays and using loops
  */
 
 // First way: separate variables
@@ -19,24 +15,3 @@ console.log(line3);
 
 let MyVariables = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 console.log(MyVariables[0] + '\n' + MyVariables[1] + '\n' + MyVariables[2]);
-/*
-// Third way: using a for loop to iterate through the array
-
-for (let i=0; i<MyVariables.length; i++){
-console.log(MyVariables[i]);
-}
-*/
-//fourth way: using forEach with a callback function
-
-/*
-1. The forEach method goes through each item in the array one by one.
-2. 'item' is the current element of the array during each iteration.
-3. This function is called a callback function because it is passed to forEach.
-4. forEach calls this function once for every element in the array.
-5. The callback function tells forEach what to do with each element,
-   because forEach itself doesn't know what to do.
-*/
-
-MyVariables.forEach(function(item) {
-  console.log(item);
-});
